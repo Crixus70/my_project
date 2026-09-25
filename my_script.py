fcbb09b9a4a57095b0b421a5470bb8fd470ca911
Cristóbal Hernández <206172803+Crixus70@users.noreply.github.com>
2026-09-25 14:29:37 -0600

    Nueva frase

--- my_script.py
+++ my_script.py
@@ -4,4 +4,6 @@ print("simples mortales")
 print(f"My working directory is {os.getcwd()}")
 print("Este es mi primer cambio")
 print("Aunque mucho me temo que no sera fácil")
+print("Pero he observado que la practica hace al maestro")
+
 
