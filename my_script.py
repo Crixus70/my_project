e5b3c5383d7c12e3e2615b70bb9bb46b009e97ff
Cristóbal Hernández <206172803+Crixus70@users.noreply.github.com>
2026-09-25 14:30:56 -0600

    El esfuerzo paga

--- my_script.py
+++ my_script.py
@@ -5,5 +5,6 @@ print(f"My working directory is {os.getcwd()}")
 print("Este es mi primer cambio")
 print("Aunque mucho me temo que no sera fácil")
 print("Pero he observado que la practica hace al maestro")
+print("y muchas veces vale la pena  el esfuerzo")
 
 
