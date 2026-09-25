af7f6b00ba005dd30a0f5e238871664fa6492c14
Cristóbal Hernández <206172803+Crixus70@users.noreply.github.com>
2026-09-25 14:26:45 -0600

    Segunda parte del escrito

--- my_script.py
+++ my_script.py
@@ -3,3 +3,5 @@ print("Hola Terrícolas")
 print("simples mortales")
 print(f"My working directory is {os.getcwd()}")
 print("Este es mi primer cambio")
+print("Aunque mucho me temo que no sera fácil")
+
